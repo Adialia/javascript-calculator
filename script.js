1c40798d65c097672060c8dd8994dd02349918ba
Adialia <skor.roksi@gmail.com>
2016-02-08 02:47:17 +0200

    evaluation of postfix

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
 	var inputString = '0';
+	var $output = $('p'); 
 
 	// event handlers
 	$('.number').on('click', addNumberToExpression);
@@ -9,7 +10,7 @@ $(document).ready(function(){
 	$('#evaluate').on('click', evaluateExpression)
 
 	function render(){
-		$('p').text(inputString);
+		$output.text(inputString);
 	}
 
 	function addNumberToExpression(){
@@ -68,10 +69,11 @@ $(document).ready(function(){
 
 	}
 
+// dot problem
 	function createTokenArray(string){
-		console.log("createTokenArray");
+		//console.log("createTokenArray");
 		var array = [];
-		var re = /[+-\//*]/g;
+		var re = /[+-\/\*]/g;
 		var tokenArray = string.split(re);
 
 		//console.log(tokenArray.length, tokenArray);
@@ -85,10 +87,10 @@ $(document).ready(function(){
 				array.push(string[opIndex]);
 			}
 			tokenArray.shift();
-			console.log("ONE!!!", array);
+			//console.log("ONE!!!", array);
 		}
 
-		console.log("final aray", array);
+		console.log("createTokenArray", array);
 		return array;
 	}
 
@@ -104,17 +106,21 @@ $(document).ready(function(){
 		//var re = /[+-\//*]/g;
 		var tokenArray = createTokenArray(infix);
 
-		console.log(infix);
-		console.log(tokenArray);
+		console.log("infix",infix);
+		//console.log(tokenArray);
 		tokenArray.forEach(function(token){
-			if (((token >= '0') && (token <= '9')) || (token == '.')){
-				postfix.push(token);
-			}
-			else if ((token == '*') || (token == '+') || (token == '-') || (token == '/')){
-				while ((opstack.length != 0) && (prec[opstack[opstack.length]] >= prec[token])){
+			if (isOperation(token)){
+				while ((opstack.length != 0) && (prec[opstack[opstack.length-1]] >= prec[token])){
+					//console.log("at leat once");
 					postfix.push(opstack.pop());
 				}
 				opstack.push(token);
+/*				console.log("opstack",opstack);
+				console.log("postfix",postfix);*/
+			}
+			else
+			{
+				postfix.push(token);
 			}
 		})
 
@@ -126,10 +132,48 @@ $(document).ready(function(){
 		return postfix;
 	}
 
-	function evaluatePostfix(postfixArray){
-		
+	function isOperation(string){
+		return ((string == '*') || (string == '+') || (string == '-') || (string == '/'))
 	}
 
-	console.log(infixToPostfix("10+5*2"));
+	function evaluatePostfix(postfixArray){
+		//console.log("postfixArray",postfixArray);
+		var operandStack = [];
+
+		postfixArray.forEach(function(token){
+			if (isOperation(token)){
+				var operand2 = operandStack.pop();
+            	var operand1 = operandStack.pop();
+            	var result = doMath(token,operand1,operand2);
+            	//console.log(token,operand1,operand2);
+            	operandStack.unshift(result);
+            	//console.log("operandStack",operandStack);
+			}
+			else{
+				operandStack.push(parseFloat(token));
+			}
+		});
+		return operandStack.pop();
+	};
+
+	function doMath(op,op1,op2){
+		if (op === "*"){
+			return op1 * op2;
+		}
+		else if (op === "/"){
+			return op1 / op2;
+		}
+		else if (op === "+"){
+			return op1 + op2;
+		}
+		else if (op === "-"){
+			return op1 - op2;
+		}
+	}
+
+	var array = infixToPostfix("10+5*2+12/3");
+	console.log(array);
+	var res = evaluatePostfix(array);
+	console.log(res);
 
 });
